7fe4f0581c33558892b069331f140c80c1380f03
L1vm4rrry <168626298@qq.com>
2026-08-28 21:55:55 +0800

    Add files via upload

--- articles.js
+++ articles.js
@@ -107,6 +107,12 @@ const pendingArticles = [
         date: "2026-08-29"
     }
     */
+    {
+        title: "关于“高中男女混寝提高生育率”玩笑的综合分析",
+        url: "articles/bin/关于“高中男女混寝提高生育率”玩笑的综合分析.html",
+        author: "Dr.wang",
+        date: "2026-08-28"
+    }
 ];
 
 // HGS奖获奖论文集
